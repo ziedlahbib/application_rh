460ec92e170e1ceeb3a55e9b881350fc91de79e9
ziedlahbib <lahbibmedzied@gmail.com>
2022-12-24 17:02:24 +0100

    proxy

--- src/app/service/user-service.service.ts
+++ src/app/service/user-service.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from 'app/models/user.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() { }
+  registartionurlUrl="/api/registration";
+  getbyusernameurl="/api/user/get-userbyusername";
+  updateuserUrl="/api/user/update-utilisateur";
+  ajoutuserUrl="/api/user/inscription";
+  uploadfilef="/api/File/uploadf";
+  getfiledetail="/api/File/filesdetail";
+  getusersUrl="/api/user/get-users";
+  getuserbyidUrl="/api/user/get-user"
+  constructor(private http : HttpClient) { }
+  registration(user :User): Observable<User>{
+    return this.http.post<User>(`${this.registartionurlUrl}`,user);
+  }
 }
