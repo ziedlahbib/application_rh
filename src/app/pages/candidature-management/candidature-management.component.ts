3468b594d36593fd3d89bc09788603a67c9d402d
ziedlahbib <lahbibmedzied@gmail.com>
2022-12-26 02:08:30 +0100

    update canditature managment

--- src/app/pages/candidature-management/candidature-management.component.ts
+++ src/app/pages/candidature-management/candidature-management.component.ts
@@ -3,6 +3,9 @@ import { Candidature } from 'app/models/candidature.model';
 import {AfterViewInit, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table'
+import { CandidatureServiceService } from 'app/service/candidature-service.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-candidature-management',
@@ -16,9 +19,37 @@ export class CandidatureManagementComponent implements OnInit {
   dataSource: MatTableDataSource<Candidature>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  constructor() { }
+  constructor(private cs:CandidatureServiceService,private toastr : ToastrService,private router:Router) { }
 
   ngOnInit(): void {
+    this.cs.getcandidatures().subscribe(
+      data=>{
+        this.listofcandidature=data;
+        //this.listofuserPAgination=this.listofuser.slice(this.start, this.end);
+        this.dataSource=new MatTableDataSource(this.listofcandidature);
+        this.dataSource.paginator = this.paginator;
+
+      }
+    )
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  supprimer(candidature :any){
+    this.cs.deletecanditature(candidature.CandidatureId).subscribe(()=>this.cs.getcandidatures().subscribe(
+      data=>{
+        this.listofcandidature=data;
+        this.dataSource = new MatTableDataSource(this.listofcandidature);
+        this.toastr.error('assurance deleted sucessfuly ','assurance deleted sucessfuly ');
+       let audio = new Audio()
+       audio.src= "../assets/alert.mp3"
+       audio.src= "../assets/confirm2.mp3"
+       audio.load();
+       audio.play();
+      }
+    )
+    );
+  }
 }
